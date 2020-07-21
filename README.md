5a2ac6357b3efb2a10076333a4b242462656ce4c
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-21 21:54:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Instagram Story with Vue Example
+
+Show you how to build Instagram Story with Vue.
+
+## Features
+
+- Timer and timeline on the top
+- Hold to pause current slide
+- Tap to navigate between slides
+- Swipe to navigate between stories
+
+## Project setup
+
+```
+yarn
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
---
+++ src/components/Story.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="story" :style="{ background: slides[currentSlideIndex] }">
+    <div class="timeline">
+      <div class="slice" v-for="(slide, i) in slides" :key="i">
+        <div class="progress">&nbsp;</div>
+      </div>
+    </div>
+    <div class="slide">
+      <p>{{ slides[currentSlideIndex] }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import anime from "animejs/lib/anime.es.js";
+import Hammer from "hammerjs";
+import { EventBus } from "../helpers/EventBus.js";
+
+export default {
+  name: "Story",
+  props: {
+    slides: Array,
+    slideDuration: Number
+  },
+  data() {
+    const timeline = anime.timeline({
+      autoplay: false,
+      duration: this.slideDuration,
+      easing: "linear"
+    });
+
+    return {
+      currentSlideIndex: 0,
+      isActive: false,
+      timeline: timeline
+    };
+  },
+  methods: {
+    activate: function() {
+      // Start timer
+      this.resetSlide();
+    },
+    deactivate: function() {
+      this.timeline.pause();
+    },
+    resetSlide: function() {
+      // Jump to beginning of the slide
+      this.timeline.pause();
+      this.timeline.seek(this.currentSlideIndex * this.slideDuration);
+      this.timeline.play();
+    },
+    nextSlide: function() {
+      if (this.currentSlideIndex < this.slides.length - 1) {
+        this.currentSlideIndex++;
+        this.resetSlide();
+      } else {
+        this.nextStory();
+      }
+    },
+    previousSlide: function() {
+      if (this.currentSlideIndex > 0) {
+        this.currentSlideIndex--;
+        this.resetSlide();
+      } else {
+        this.previousStory();
+      }
+    },
+    nextStory: function() {
+      EventBus.$emit("NEXT_STORY");
+    },
+    previousStory: function() {
+      EventBus.$emit("PREVIOUS_STORY");
+    }
+  },
+  mounted() {
+    let [$timeline] = this.$el.getElementsByClassName("timeline");
+
+    // Add progress bars to the timeline animation group
+    this.slides.forEach((color, index) => {
+      const trg = $timeline.getElementsByClassName("slice")[index];
+      this.timeline.add({
+        targets: trg.getElementsByClassName("progress"),
+        width: "100%",
+        changeBegin: () => {
+          // Update the Vue componenet state when progress bar begins to play
+          this.currentSlideIndex = index;
+        },
+        complete: () => {
+          // Move to the next story when finished playing all slides
+          if (index === this.slides.length - 1) {
+            this.nextStory();
+          }
+        }
+      });
+    });
+
+    this.hammer = new Hammer.Manager(this.$el, {
+      recognizers: [
+        [Hammer.Pan, { direction: Hammer.DIRECTION_HORIZONTAL }],
+        [Hammer.Tap],
+        [Hammer.Press, { time: 1, threshold: 1000000 }]
+      ]
+    });
+
+    this.hammer.on("press", () => {
+      this.timeline.pause();
+    });
+
+    this.hammer.on("pressup tap", () => {
+      this.timeline.play();
+    });
+
+    // Tap on the side to navigate between slides
+    this.hammer.on("tap", event => {
+      if (event.center.x > window.innerWidth / 3) {
+        this.nextSlide();
+      } else {
+        this.previousSlide();
+      }
+    });
+
+    // Handle swipe
+    this.hammer.on("pan", event => {
+      if (event.isFinal) {
+        if (event.deltaX < 0) {
+          this.nextStory();
+        } else if (event.deltaX > 0) {
+          this.previousStory();
+        }
+      }
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.story {
+  float: left;
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.timeline {
+  display: flex;
+  flex-grow: 0;
+  width: 100%;
+
+  box-sizing: border-box;
+  padding: 5px;
+}
+
+.timeline > .slice {
+  background: rgba(0, 0, 0, 0.25);
+  height: 4px;
+  margin: 0 2px;
+  width: 100%;
+}
+.timeline > .slice:first-child {
+  margin-left: 0;
+}
+
+.timeline > .slice:last-child {
+  margin-right: 0;
+}
+
+.timeline > .slice > .progress {
+  background: white;
+  height: 4px;
+  width: 0%;
+}
+
+.slide {
+  /* Take the rest of the page */
+  flex-grow: 1;
+
+  /* Center align */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.slide p {
+  font-size: 60px;
+  opacity: 0.5;
+}
+</style>
